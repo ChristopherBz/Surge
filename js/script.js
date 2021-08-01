0ac4cb16dbf21928620fe50d6dd3af4c9d42f378
ChristopherBz <81110745+ChristopherBz@users.noreply.github.com>
2021-08-01 14:38:35 +1000

    header

--- js/script.js
+++ js/script.js
@@ -1781,7 +1781,20 @@
 
 }());
 
+$('#modal1').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+});
 
+$('#modal6').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal6 iframe').attr("src", $("#modal6 iframe").attr("src"));
+});
+
+$('#modal4').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal4 iframe').attr("src", $("#modal4 iframe").attr("src"));
+});
 
 // Ipad/Iphone fix for double clicking links
 if(/iPhone|iPad/i.test(navigator.userAgent)) {
